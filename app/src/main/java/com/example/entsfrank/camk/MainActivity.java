badfb4125979380d3f83f47c57401adf29faa897
skafis <franciskipchumba5@gmail.com>
2016-03-21 12:55:48 +0300

    making the button to capture images

--- app/src/main/java/com/example/entsfrank/camk/MainActivity.java
+++ app/src/main/java/com/example/entsfrank/camk/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.entsfrank.camk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+
+    // directory name to store captured images and videos
+    private static final String IMAGE_DIRECTORY_NAME = "Hello Camera";
+
+    private Uri fileUri;  //file Url to store image
+    private ImageView imgPreview;
+    private Button btnCapturePicture;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgPreview = (ImageView)findViewById(R.id.imgPreview);
+        btnCapturePicture = (Button)findViewById(R.id.btnCapturePicture);
+
+    // capture image click event
+        btnCapturePicture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //capture image
+                captureImage();
+            }
+        });
     }
 }
